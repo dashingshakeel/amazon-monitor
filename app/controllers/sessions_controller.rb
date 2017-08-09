a1d13a896aa85596b36f22e017b74d2e53643dd5
dashingshakeel <shkeelrauf@gmail.com>
2017-08-09 18:41:56 +0500

    Form for session added.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+   
+    else
+      flash[:danger] = 'Invalid Email and password'
+      render 'new'
+
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get '/signup' => 'users#new'
   post 'signup'  => 'users#create'
-
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
